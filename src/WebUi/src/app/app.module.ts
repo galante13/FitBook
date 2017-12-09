ac7821238479a4e797a1bdae8965dd5887e273b6
Gustaw Wypych <gustaw.wypych@gmail.com>
2017-12-09 11:32:58 +0100

    [WebUi] Adding material desing

--- src/WebUi/src/app/app.module.ts
+++ src/WebUi/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import "hammerjs";
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
