6bdc0c337eef5fc979982952abe932ce005befb0
Gustaw Wypych <gustaw.wypych@gmail.com>
2017-12-10 12:48:24 +0100

    [WebUi] Adding sidenav with app menu

--- src/WebUi/src/app/app.module.ts
+++ src/WebUi/src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 import 'hammerjs';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatButtonModule, MatFormFieldModule, MatNativeDateModule, MatDatepickerModule } from '@angular/material';
+
+import {
+  MatInputModule, MatButtonModule, MatFormFieldModule,
+  MatNativeDateModule, MatDatepickerModule,
+  MatIconModule, MatCardModule, MatSidenavModule,
+  MatListModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { AddTrainerComponent } from './trainers/components/add-trainer/add-trainer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 @NgModule({
@@ -20,19 +27,28 @@ import { AddTrainerComponent } from './trainers/components/add-trainer/add-train
     AppComponent,
     MenuComponent,
     AddTrainerComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
     FormsModule,
+
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
     MatNativeDateModule,
     MatDatepickerModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
